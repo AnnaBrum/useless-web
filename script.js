07e7d4d385c25e80e81f9721e26c75e4a7b50c3e
AnnaBrum <annahelenabrumark@gmail.com>
2022-12-06 15:26:57 +0100

    Created image-carousel

--- script.js
+++ script.js
@@ -1,47 +1,47 @@
 /* USELESS-WEB REQUIREMENTS
-1. 
+1.  1 av 2
 The project should use at least two types of event listeners for example scroll, click and keypress.
 
-2. 
+2.  0
 The project should use some kind of iteration for example map, forEach or for.
 
-3. 
+3.  Klar
 The project should use arrays or objects or both to store values.
 
-4. 
+4.
 The project should have a nice user interface with consistent margins and paddings.
 
-5. 
+5.
 The project should use CSS variables for colors and font families..
 
-6. 
+6.
 The project should use at least one self hosted font with woff and woff2 format.
 
-7. 
+7.
 The project should be responsive and be built using the method mobile-first.
 
-8. 
+8.
 The project should use responsive images when using images.
 
-9. 
+9.
 The project should implement an accessible graphical user interface.
 
-10. 
+10.
 The repository should be created from the template repository yrgo/template-useless-web
 
-11. 
+11.
 The project must not include any coding errors, warning or notices.
 
-12. 
+12.
 The project must be tested on at least two of your classmates computers. Add the testers names to the README.md file.
 
-13. 
+13.
 The project should be deployed to either Netlify or Vercel
 
-14. 
+14.
 The application should be pushed to a public repository on GitHub.
 
-15. 
+15.
 The application should be useless 🙅‍♀️ */
 
 /* STAR-CONTAINER */
@@ -49,7 +49,7 @@ function random(min, max) {
   return min + Math.random() * (max + 1 - min);
 }
 
-const starContainer = document.querySelector(".star-container");
+const starContainer = document.querySelector('.star-container');
 const canvasSize = starContainer.offsetWidth * starContainer.offsetHeight;
 const starsFraction = canvasSize / 2000;
 
@@ -59,60 +59,46 @@ for (let i = 0; i < starsFraction; i++) {
   let yAxis = random(0, 100);
   let alpha = random(0.5, 1);
   let size = random(1, 2);
-  let colour = "#ffffff";
+  let colour = '#ffffff';
 
   // Add them to the body
-  const star = document.createElement("div");
-  star.style.position = "relative";
-  star.style.left = xAxis + "%";
-  star.style.top = yAxis + "%";
+  const star = document.createElement('div');
+  star.style.position = 'relative';
+  star.style.left = xAxis + '%';
+  star.style.top = yAxis + '%';
   star.style.opacity = alpha;
-  star.style.width = size + "px";
-  star.style.height = size + "px";
+  star.style.width = size + 'px';
+  star.style.height = size + 'px';
   star.style.backgroundColor = colour;
   starContainer.appendChild(star);
 }
 
-
-/* Array of items */
+/* Array of items which loops when being clicked*/
 const items = [
-  { name: "bananas", image: "./images/bananas.png" },
-  { name: "chair", image: "./images/chair.png" },
-  { name: "dog", image: "./images/dog.png" },
-  { name: "hen", image: "./images/hen.png" },
-  { name: "money", image: "./images/money.png" },
+  './images/bananas.png',
+  './images/chair.png',
+  './images/dog.png',
+  './images/hen.png',
+  './images/money.png',
 ];
 
-const itemContainer = document.querySelector(".item-container");
+const itemContainer = document.querySelector('.item-container');
+const item = document.querySelector('#item')
+
+let currentImageIndex = 0;
+item.addEventListener('click', () => {
+  item.classList.add('move');
+  item.src = items[currentImageIndex];
+  currentImageIndex++;
+
+  if (currentImageIndex > items.length -1) {
+    currentImageIndex = 0;
+  }
+})
 
 
-
-itemContainer.addEventListener('click', () => {
-  items.forEach(item => {
-
-    const stringToHTML = (str) => {
-      const div = document.createElement("div");
-      div.innerHTML = str;
-      return div.firstChild;
-    };
-    
-    const createItem = (icon, name) => {
-      return `<div class="item ${name}">
-                <img src="${icon}">
-              </div>`;
-    };
-
-    itemContainer.appendChild(stringToHTML(element));
-  });
-});
-
-generateItems();
-
-let i = 0;
-
-    button.addEventListener ("click", () => {
-        background.style.backgroundColor= colors[i++];
-        if (i > colors.length) {
-            i = 0;
-        }
-    });
+// function moveImage(e) {
+//   item.style.position = "relative";
+//   item.style.top = "100px";
+//   e.preventDefault();
+// }
