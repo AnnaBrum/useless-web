c025ae2b383303d53cfee3573bf063bc6e4af3e7
AnnaBrum <annahelenabrumark@gmail.com>
2022-12-07 22:44:43 +0100

    Added glow-effect and some more pics

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 /* USELESS-WEB REQUIREMENTS
-1.  1 av 2
+1. check
 The project should use at least two types of event listeners for example scroll, click and keypress.
 
-2.  0
+2.  check
 The project should use some kind of iteration for example map, forEach or for.
 
-3.  Klar
+3.  check
 The project should use arrays or objects or both to store values.
 
-4.
+4.  check ?
 The project should have a nice user interface with consistent margins and paddings.
 
-5.
+5. check ?
 The project should use CSS variables for colors and font families..
 
-6.
+6. check
 The project should use at least one self hosted font with woff and woff2 format.
 
-7.
+7. check ?
 The project should be responsive and be built using the method mobile-first.
 
-8.
+8. check ?
 The project should use responsive images when using images.
 
 9.
@@ -72,11 +72,12 @@ for (let i = 0; i < starsFraction; i++) {
   star.style.backgroundColor = colour;
   starContainer.appendChild(star);
 
-  // Makes stars glow on scroll
-  starContainer.addEventListener('click', (event) => {
+// Makes stars glow on scroll
+  window.addEventListener('scroll', (event) => {
     star.classList.toggle('glow');
   })
 }
+
 
 
 /* Array of items which loops when being clicked*/
@@ -86,6 +87,12 @@ const items = [
   './images/dog.png',
   './images/hen.png',
   './images/money.png',
+  './images/cheese.png',
+  './images/musk.png',
+  './images/mug.png',
+  './images/rose.png',
+  './images/shoe.png',
+
 ];
 
 const itemContainer = document.querySelector('.item-container');
@@ -112,12 +119,12 @@ function resetImage(imageIndex) {
 }
 
 itemContainer.addEventListener('click', () => {
-  /* first click hides current image */
+  /* first click adds class move to image */
   images[currentImageIndex].classList.add('move');
   resetImage(currentImageIndex);
   /* then moves on to next image in count */
   currentImageIndex++;
-  /*  */
+  /* Starts back on first image after the last is clicked */
   if (currentImageIndex > images.length -1) {
     currentImageIndex = 0;
   }
